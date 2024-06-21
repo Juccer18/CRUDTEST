e3e406705c5e124481f7e825b4d788b9ac3705c8
Juccer18 <112097136+Juccer18@users.noreply.github.com>
2024-06-20 20:17:01 -0700

    First commit

---
+++ CRUDTEST/Models/CRUDMVCContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUDTEST.Models
+{
+    public partial class CRUDMVCContext : DbContext
+    {
+        public CRUDMVCContext()
+        {
+        }
+
+        public CRUDMVCContext(DbContextOptions<CRUDMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Client> Clients { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_general_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnType("int(11)");
+
+                entity.Property(e => e.Description).HasMaxLength(45);
+
+                entity.Property(e => e.Name).HasMaxLength(35);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CRUDTEST/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDTEST.Models
+{
+    public partial class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateOnly? Register { get; set; }
+    }
+}
